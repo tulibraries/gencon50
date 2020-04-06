6bf8fb7da1561c81de203659f6a0c092313033c5
Steven Ng <steven@nomadicoder.com>
2020-04-06 14:41:04 -0400

    Verify upload with login

---
+++ spec/features/upload_with_authentication_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "UploadWithAuthentications", type: :feature do
+  before(:all) do
+    @email = "admin@example.eom"
+    @password = "password"
+    @user = User.create!(email: @email, password: @password, password_confirmation: @password)
+  end
+
+  after(:all) do
+    @user.destroy
+  end
+
+  context "Access upload page" do
+    scenario "Create new item " do
+      visit("/upload")
+      expect(page).to_not have_xpath("//body[contains(., 'New Upload')]")
+      expect(page).to have_xpath("//main[contains(., 'Log in')]")
+      
+      fill_in("Email", with: @email)
+      fill_in("Password", with: @password)
+      click_button("Log in")
+      expect(page).to have_xpath("//body[contains(., 'New Upload')]")
+    end
+  end
+end
