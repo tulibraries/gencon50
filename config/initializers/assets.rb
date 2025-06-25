abee817b1e89ceb54fe553ea90c8d4eb583d6ba2
Chris Doyle <cdoyle@temple.edu>
2025-06-25 14:25:57 -0400

    reset asset paths for sprockets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,8 @@ Rails.application.config.assets.version = "1.0"
 
 Rails.application.config.assets.paths = [ Rails.root.join("app/assets/builds") ]
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
+Rails.application.config.assets.paths << Rails.root.join("/app/assets/fonts")
+Rails.application.config.assets.precompile = %w[ application.css application.js ]
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
