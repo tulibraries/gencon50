f156b52935f5949731acda233006305599a0703b
Chris Doyle <cdoyle@temple.edu>
2025-06-25 14:12:38 -0400

    relies on bootstrap cdn, consolidates remaining js.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,2 @@
-// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
 import "blacklight-frontend/app/javascript/blacklight"
-import "bootstrap/dist/js/bootstrap.bundle"
