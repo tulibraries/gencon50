6217ba1df1ffde917b42a2d6bbff3bacc8655fe1
Chris Doyle <cdoyle@temple.edu>
2025-09-18 11:33:54 -0400

    create lint test and deploys

---
+++ .github/scripts/deploy-qa.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+echo "Starting QA deployment..."
+
+cd ..
+# clone deployment playbook
+git clone --single-branch --branch main git@github.com:tulibraries/ansible-playbook-gencon50.git
+cd ansible-playbook-gencon50
+
+# install playbook requirements
+pipenv install
+# install playbook role requirements
+pipenv run ansible-galaxy install -r requirements.yml
+
+# setup vault password
+echo "$ANSIBLE_VAULT_PASSWORD" > ~/.vault
+
+# deploy to qa using ansible-playbook
+echo "Running: pipenv run ansible-playbook -i inventory/qa playbook.yml --vault-password-file=~/.vault"
+pipenv run ansible-playbook -i inventory/qa playbook.yml --vault-password-file=~/.vault
+
+echo "QA deployment completed successfully!"
