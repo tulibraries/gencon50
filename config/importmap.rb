2772a761f1da01d9bdefcc44656658a959875b8d
Steven Ng <steven.ng@temple.edu>
2026-05-01 14:10:09 -0600

    Vendor importmap JavaScript dependencies

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,6 +4,7 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "blacklight", to: "blacklight/index.js", preload: true
 pin_all_from "vendor/javascript/blacklight", under: "blacklight"
-pin "@github/auto-complete-element", to: "https://cdn.skypack.dev/@github/auto-complete-element"
-pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/esm/popper.js", preload: true
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.esm.min.js", preload: true
+pin "@github/auto-complete-element", to: "@github--auto-complete-element.js" # @3.8.0
+pin "@popperjs/core", to: "@popperjs--core.js", preload: true # @2.11.8
+pin "bootstrap", preload: true # @5.3.7
+pin "@github/combobox-nav", to: "@github--combobox-nav.js" # @2.3.1
